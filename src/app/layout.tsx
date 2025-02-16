8a2b5ea1d82d455f79bc2143fa1eb58b4c577cae
Pitu <heyitspitu@gmail.com>
2025-02-16 20:50:28 -0300

    feat: add window size indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
 import { Providers } from '@/app/providers';
 import './globals.css';
+import { TailwindIndicator } from '@/components/TailwindIndicator';
 
 export const metadata: Metadata = {
 	title: 'Starter template',
@@ -10,9 +11,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
+			<head />
 			<body>
 				<Providers>{children}</Providers>
+				<TailwindIndicator />
 			</body>
 		</html>
 	);
