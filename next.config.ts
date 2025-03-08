71ad41ed0cddaa7523a4bd81e3a325380b6a59c7
Kana <heyitspitu@gmail.com>
2025-03-08 12:35:29 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	experimental: {
+		reactCompiler: true
+	},
 	reactStrictMode: true,
 	output: 'standalone',
 	pageExtensions: ['ts', 'tsx'],
